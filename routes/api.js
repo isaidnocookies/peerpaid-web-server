d646f035bb3a10dfd5fe0c96182dd8bf261444f6
Michael Sweeney <jfmike@gmail.com>
2017-06-24 17:42:31 -0700

    added api route

---
+++ routes/api.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const request = require('request')
+const config = require('config')
+const jwt = require('express-jwt')
+
+/* GET users listing. */
+router.post('/token', function(req, res, next) {
+    if (req.body.username !== '' && req.body.password !== '') {
+        console.log(req.body.username, req.body.password)
+        const options = {
+            method: 'post',
+            body: {username: req.body.username, password: req.body.password},
+            json: true,
+            url: config.get('dataServer') + '/login'
+        }
+        request(options, function(error, response, body) {
+            console.log(body)
+            if (error) {
+                res.json({
+                    success: false
+                })
+            } else {
+                if (body.success) {
+                    res.json({
+                        success: true
+                    })
+                } else {
+                    res.json({
+                        success: false
+                    })
+                }
+            }
+        })
+    }
+});
+
+module.exports = router;
