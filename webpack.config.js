827d12a0772a52357435a4737cb77a3e1d24e5be
Volure DarkAngel <volure@loki-systems.com>
2017-11-03 13:12:09 -0700

    Updated webpack to allow jpg, png and ico files to be downloaded directly from the server.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,14 @@ var path = require('path');
 var npmFolder = path.resolve(__dirname, '../node_modules');
 
 module.exports = {
+  module: {
+    loaders: [
+      {
+        test: /\.(png|jpg|ico)$/,
+        loader: 'url?limit=25000'
+      }
+    ]
+  },
   resolve: {
     alias: {
       // Ensure compatibility to original bootstrap
