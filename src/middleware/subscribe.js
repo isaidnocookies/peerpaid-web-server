dee3fb51059c9e82b89f69b4b55fb20d168a13d5
ar-to <aricode0@gmail.com>
2017-12-29 12:36:34 -0800

    completed mailchimp subscription post request via /subscribe middleware

--- src/middleware/subscribe.js
+++ src/middleware/subscribe.js
@@ -5,24 +5,23 @@ var mailchimp = new Mailchimp(api_key);
 
 module.exports = function (options = {}) {
   return function subscribe(req, res, next) {
-    console.log('subscribe middleware is running');
-    console.log('req for /subscribe: ', req.body.email);
+    // console.log('subscribe middleware is running');
+    // console.log('req for /subscribe: ', req.body.email);
 
-    // mailchimp.get({
-    //   path: '/lists/886d3802d8/members/60b23f3969f08077956f6fb4846dfaf4'
-    // })
     mailchimp.post('/lists/886d3802d8/members', {
-      email_address : req.body.email,
-      status : 'subscribed'
+      email_address: req.body.email,
+      status: 'subscribed'
     })
       .then(function (response) {
-        console.log('response for mailchimp POST: ',response);
+        // console.log('response for mailchimp POST: ',response);
+        console.log('New member subscribed to mailchimp :)');
       })
       .catch(function (error) {
-        console.log('mailchimp error: ',error);
+        console.log('mailchimp error, member not subscribed: ', error);
       });
 
-
-    next();
+    // res.redirect('/coming-soon');
+    res.send({success: true});
+    // next();
   };
 };
