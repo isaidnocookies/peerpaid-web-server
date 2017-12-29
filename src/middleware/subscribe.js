63fa65a16a46d2199884207451036b6940186688
ar-to <ricode0@gmail.com>
2017-12-29 10:33:39 -0800

    added mailchimp post to peerpaid list

--- src/middleware/subscribe.js
+++ src/middleware/subscribe.js
@@ -1,6 +1,28 @@
+var Mailchimp = require('mailchimp-api-v3');
+
+var api_key = '77ffe8c31d60c9375ba6d44f1ed91693-us17';
+var mailchimp = new Mailchimp(api_key);
+
 module.exports = function (options = {}) {
   return function subscribe(req, res, next) {
     console.log('subscribe middleware is running');
+    console.log('req for /subscribe: ', req.body.email);
+
+    // mailchimp.get({
+    //   path: '/lists/886d3802d8/members/60b23f3969f08077956f6fb4846dfaf4'
+    // })
+    mailchimp.post('/lists/886d3802d8/members', {
+      email_address : req.body.email,
+      status : 'subscribed'
+    })
+      .then(function (response) {
+        console.log('response for mailchimp POST: ',response);
+      })
+      .catch(function (error) {
+        console.log('mailchimp error: ',error);
+      });
+
+
     next();
   };
 };
